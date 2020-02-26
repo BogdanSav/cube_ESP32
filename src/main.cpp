26f4f7730b98c3748647109281ff9204aff09eae
BogdanSav <bohdan16@live.com>
2020-02-26 08:43:09 +0200

    ported websoket

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <SPIFFS.h>
-#include <FS.h>
-#include <WebServer.h>
+//#include <SPIFFS.h>
+//#include <FS.h>
+//#include <WebServer.h>
 #include <SPI.h>
 #include <LedMatrix.h>
 #include<Ticker.h>
+#include<ArduinoJson.h>
+#include<WebSocketsClient.h>
+#include<SocketIOclient.h>
 //defines
 //
 #define NUMBER_OF_DEVICES 1
@@ -19,12 +22,15 @@
 //
 int first;
 int second;
+const char* message;
+const char* ssid ="ChNU-WiFi-NET";
 //bool rst  =true;
 //
 //classes
 //
+WebSocketsClient web_sockets;
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES,CS_PIN);
-WebServer server(80);
+//WebServer server(80);
 //
 //functions
 //
@@ -50,12 +56,70 @@ void inv_t_right();
 void inv_t_left();
 Ticker tr(t_right, 100);
 Ticker tl(t_left, 100);
-//
 
+//
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+	const uint8_t* src = (const uint8_t*) mem;
+	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+	for(uint32_t i = 0; i < len; i++) {
+		if(i % cols == 0) {
+			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+		}
+		Serial.printf("%02X ", *src);
+		src++;
+	}
+	Serial.printf("\n");
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+	switch(type) {
+		case WStype_DISCONNECTED:
+			Serial.printf("[WSc] Disconnected!\n");
+			break;
+		case WStype_CONNECTED:
+			Serial.printf("[WSc] Connected to url: %s\n", payload);
+
+			// send message to server when Connected
+			web_sockets.sendTXT("Connected");
+			break;
+		case WStype_TEXT:
+		//	Serial.printf("[WSc] get text: %s\n", payload);
+			message = (char*)payload;
+			// send message to server
+			// web_sockets.sendTXT("message here");
+			break;
+		case WStype_BIN:
+			Serial.printf("[WSc] get binary length: %u\n", length);
+			hexdump(payload, length);
+
+			// send data to server
+			// web_sockets.sendBIN(payload, length);
+			break;
+		case WStype_ERROR:			
+		case WStype_FRAGMENT_TEXT_START:
+		case WStype_FRAGMENT_BIN_START:
+		case WStype_FRAGMENT:
+		case WStype_FRAGMENT_FIN:
+			break;
+	}
+
+}
 
 void setup() {
-  WiFi.softAP("Cube_Controls"); //wifi enable
-  SPIFFS.begin();
+  Serial.begin(115200);
+  WiFi.begin(ssid);
+   while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  //SPIFFS.begin();
+  
   tr.start();
   tl.start();
   ledMatrix.init();
@@ -65,7 +129,12 @@ void setup() {
   pinMode(wheel_l_2,OUTPUT);
   pinMode(wheel_r_1,OUTPUT);
   pinMode(wheel_r_2,OUTPUT);
+   web_sockets.beginSSL("robo-dot.akit.pp.ua", 8999,"/");
+    web_sockets.onEvent(webSocketEvent);
+    web_sockets.setReconnectInterval(5000);
 
+  
+/*
    server.on("/sended", []()
   {
     server.send(200, "text/html", "message recived");
@@ -125,13 +194,38 @@ void setup() {
   });
 
 
-  server.begin();
+  server.begin();*/
 }
 
 
 void loop() {
-  server.handleClient();
+  StaticJsonDocument<2048> doc;
+	DeserializationError root = deserializeJson(doc,message);
+
+	//Serial.println(root.c_str());
+  float angle = doc["angle"]["degree"];
+  float force = doc["force"];
+Serial.println(angle);
+Serial.println(force);
+  if(force>=0.5){
+    if(45.0<angle<135){
+      move_fow();
+    }
+    else if(135<angle<225){
+      move_left();
+    }
+    else if(225<angle<315){
+      move_back();
+    }
+    else if(angle<45&&315<angle){
+      move_right();
+    }
+  }
+ // delay(1000);
+  web_sockets.loop();
+  //server.handleClient();
 }
+
 String setpixel(int first, int second)
 {
 
@@ -253,7 +347,7 @@ void t_left() {
 void t_right() {
   move_right();
 }
-
+/*
 String getContentType(String filename) { // convert the file extension to the MIME type
   if (filename.endsWith(".htm")) return "text/html";
   else if (filename.endsWith(".html")) return "text/html";
@@ -283,4 +377,4 @@ bool handleFileRead(String path) { // send the right file to the client (if it e
   }
   Serial.println("\tFile Not Found");
   return false;                                         // If the file doesn't exist, return false
-}
+}*/
